6c682a259f1f6ddb924c851f97e229bbe4020a33
cfd2474 <m.leckliter@cfa3757.org>
2026-02-23 22:51:57 -0800

    about info update

--- install/install.sh
+++ install/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# TAKNET-PS-ADSB-Feeder One-Line Installer v2.59.23
+# TAKNET-PS-ADSB-Feeder One-Line Installer v2.59.24
 # curl -fsSL https://raw.githubusercontent.com/cfd2474/TAKNET-PS_ADS-B_Feeder/main/install/install.sh | sudo bash
 
-INSTALLER_VERSION="2.59.23"
+INSTALLER_VERSION="2.59.24"
 
 set -e
 
